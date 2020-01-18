0902d1b32763cf525b166ec1c61a35ae4db3876a
Dao Hoang Son <daohoangson@gmail.com>
2020-01-18 15:25:35 +0700

    Add support for client certificate authentication

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 import 'package:k8sapi/api.dart';
@@ -8,6 +10,8 @@ abstract class Db {
 
 class Cluster {
   final String certificateAuthority;
+  final List<int> clientCertificate;
+  final List<int> clientKey;
   final String name;
   final String server;
   final String token;
@@ -18,9 +22,20 @@ class Cluster {
   Cluster(
     this.name, {
     this.certificateAuthority,
+    this.clientCertificate,
+    this.clientKey,
     this.server,
     this.token,
-  });
+  })  : assert(certificateAuthority != null),
+        assert(server != null),
+        assert(
+          (clientCertificate == null) == (clientKey == null),
+          'Client certificate and key must be provided together',
+        ),
+        assert(
+          (clientCertificate == null) != (token == null),
+          'Client certificate and token cannot be used together',
+        );
 
   K8sapi get api {
     _api ??= K8sapi(dio: dio);
@@ -29,13 +44,29 @@ class Cluster {
 
   Dio get dio {
     if (_dio == null) {
-      _dio = Dio(BaseOptions(
-        baseUrl: server,
-        headers: {"Authorization": "Bearer $token"},
-      ));
+      final headers = <String, String>{};
+      if (token != null) {
+        headers['Authorization'] = 'Bearer $token';
+      }
 
-      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
-          (client) => client..badCertificateCallback = (_, __, ___) => true;
+      _dio = Dio(BaseOptions(baseUrl: server, headers: headers));
+
+      final adapter = dio.httpClientAdapter as DefaultHttpClientAdapter;
+      adapter.onHttpClientCreate = (client) {
+        if (clientCertificate != null && clientKey != null) {
+          final securityContext = SecurityContext();
+          securityContext.useCertificateChainBytes(clientCertificate);
+          securityContext.usePrivateKeyBytes(clientKey);
+          client = HttpClient(context: securityContext);
+        }
+
+        if (certificateAuthority != null) {
+          // TODO: verify cert issued by CA
+          client..badCertificateCallback = (_, __, ___) => true;
+        }
+
+        return client;
+      };
     }
 
     return _dio;
